b8ced8df1307c94ffb43cdc12608b667fbfed05c
FrancinaldoSB <francinaldosousabarbosa21@gmail.com>
2025-09-01 14:26:50 -0300

    Iniciando Dataset Titanic

---
+++ src/titanic/titanic.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import statsmodels.api as sm
+import os
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
+
